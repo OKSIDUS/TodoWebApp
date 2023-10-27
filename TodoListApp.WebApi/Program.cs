810a68ebc1051e04899cf36526ea7044ad45e672
Klymenko Yaroslav <oksid123123@gmail.com>
2023-10-27 16:48:57 +0400

    add  and configure dbcontext

--- TodoListApp.WebApi/Program.cs
+++ TodoListApp.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using TodoListApp.Services.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace TodoListApp.WebApi;
 
 public class Program
@@ -12,6 +15,12 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        _ = builder.Services.AddDbContext<TodoListDbContext>((services, options) =>
+        {
+            var configuration = services.GetRequiredService<IConfiguration>();
+            _ = options.UseSqlServer(configuration.GetConnectionString("TodoList"));
+        });
 
         var app = builder.Build();
 
